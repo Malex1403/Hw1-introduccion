25f88c4ed010d974f0f4e408c0218efef23e3424
Malex1403 <129395546+Malex1403@users.noreply.github.com>
2023-03-30 14:57:11 +0000

    hello world function

---
+++ Homework/hello.py
@@ -0,0 +1,11 @@
+#################################################
+# Funciones que tenés que programar
+#################################################
+
+
+def hello_world():
+    # Modificar este código para que la función hello_world retorne el
+    # string "Hello World!"
+    print("Hello World!")
+
+hello_world()
